03cb0c7a9a987569eee2b9791bdf60c68aa6d9ba
DroxKiwi <corentinfredj.pro@gmail.com>
2023-05-02 22:39:36 +0200

    synch

--- bin/backup/backup.sh
+++ bin/backup/backup.sh
@@ -1,15 +1,16 @@
 
 #!/usr/bin/env /bin/bash
+
 # Dump the current database
 
 # Diplay commands and end script at first command fail or use of an unset variable
 set -o errexit -o nounset -o xtrace -o pipefail
 
-cd /home/dev/gitrepo/webappBeWave
+cd /home/tux/webappBeWave
 # Get Express environment variables
 source .env.prod
 
 cd bin/backup
 FILE_NAME="$(date -Ins).sql"
-pg_dump -U $USER $DATABASE > database.$DATABASE.sql
-cp database.$DATABASE.sql "$FILE_NAME"
+sudo --user postgres pg_dump $DATABASE > database.$DATABASE.sql
+cp database.$DATABASE.sql backups/"$FILE_NAME"
